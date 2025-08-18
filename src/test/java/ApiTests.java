f76676788eb5ad4b259821911dea9f758a205402
Mumukisya <Ivanchenko@secretgroup.ru>
2025-08-18 12:17:54 +0300

    Реализовал тесты для первого задания

--- src/test/java/ApiTests.java
+++ src/test/java/ApiTests.java
@@ -33,7 +33,7 @@ public class ApiTests {
     }
 
     @Test
-    @DisplayName("Проверка тела ответа при успешной регистрации")
+    @DisplayName("Проверка тела ответа при успешной регистрации1")
     public void verifyRegisterResponseBodyStructure() {
         Faker faker = new Faker();
         // 1. Подготовка тестовых данных
